5decad894d2810967b75f84831d3dd0e3c111b50
Kevin Meneses <k3.22@live.com.mx>
2017-11-25 13:06:08 -0600

    cg

---
+++ ProyectoCG/figuras.h
@@ -0,0 +1,32 @@
+//#include <windows.h>  //Solo para Windows
+//#include <GL/gl.h>     // The GL Header File
+//#include <stdio.h>
+
+//#include <math.h>
+#include "Main.h"
+
+
+class CFiguras
+{
+	public:
+
+	float text_der;
+	float text_izq;
+
+	void esfera(GLfloat radio, int meridianos, int paralelos, GLuint text); //Funcíon creacion esfera
+	void cilindro(float radio, float altura, int resolucion, GLuint text);	//Funcíon creacion cilindro
+	void cono(float altura, float radio, int resolucion, GLuint text);		//Funcíon creacion cono
+	void prisma_anun (GLuint text, GLuint text2);
+	void skybox(float largo, float altura, float profundidad, GLuint ny, GLuint nx, GLuint pz, GLuint px, GLuint nz, GLuint py);
+	void plano(float altura, float largo, float profundidad, GLuint text, GLfloat n);
+	void hoja(float altura, float largo, float profundidad, GLuint text, GLfloat n);
+	// funcion para el cielo
+	//Funcíon creacion movimiento
+	void prisma (float altura, float largo, float profundidad, GLuint text);//Funcíon creacion prisma
+	void prisma2 (GLuint text, GLuint text2, GLfloat n);
+	
+
+	//void torus(GLfloat radioM, GLfloat radiom, int meridianos, int paralelos, GLuint text );
+	void torus(GLfloat radioM, GLfloat radiom, int meridianos, int paralelos);
+
+};
